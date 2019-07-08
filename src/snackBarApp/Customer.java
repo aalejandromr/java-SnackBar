994592b7abd76b719f9c267a6b62da634edcd6f3
Alejandro Mejia <aalejandro.mreyes@icloud.com>
2019-07-08 15:46:26 -0400

    Adds Customer and vendingMachine

---
+++ src/snackBarApp/Customer.java
@@ -0,0 +1,40 @@
+package snackBarApp;
+
+public class Customer
+{
+  public static int maxId = 0;
+  private int id;
+  private String name;
+  private double cash;
+
+  public Customer(String name, double cash)
+  {
+    maxId++;
+    id = maxId;
+    this.name = name;
+    this.cash = cash;
+  }
+
+  public void buySnack(double total)
+  {
+    // System.out.println("Before: $" + cash);
+    cash -= total;
+    // System.out.println("Now: $" + cash);
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public double getCashOnHand()
+  {
+    return cash;
+  }
+  
+}
